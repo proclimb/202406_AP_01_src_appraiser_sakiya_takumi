5605f87129e93784dce73a3707892f18cf73873c
sakiya takumi <202404ap01_sakiya@x-ide.sakura.ne.jp>
2024-08-19 15:24:20 +0900

    　Redmineのチケットナンバー
    　不具合状況の説明
    	・新規登録時データベースに登録されない
    	・検索結果が表示されない
    　修正内容の説明
    	・「除外」チェックボックスを未チェックにした際のsDelの値が正しく1に設定されるよう論理式を修正
    	・fnsqlArticleInsert()の引数の順番を変更
    	・fusqlArticleInsert()の引数flgの値を0→1に変更
    	・SELECT文の取得カラム名のスペルミス修正
    	・検索結果の1ページ目を取得する際、200件目から100件取得するように設定されていたため、0件目から100件取得するようにLIMIT句を修正

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -19,12 +19,12 @@ function subArticle()
 	$orderTo = $_REQUEST['orderTo'];
 	$sPage   = $_REQUEST['sPage'];
 
-	if ($sDel = '') {
+	if ($sDel == '') {
 		$sDel = 1;
 	}
 
 	if (!$sPage) {
-		$sPage = l;
+		$sPage = 1;
 	}
 
 	if (!$orderBy) {
@@ -85,9 +85,9 @@ function subArticle()
 	if ($_REQUEST['act'] == 'article') {
 		return;
 	}
-	$sql = fnSqlArticleList(0, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleNote, $sKeyBox, $sDrawing, $sSellCharge, $sPage, $orderBy, $orderTo);
-	$res = mysql_query($sql);
-	$row = mysql_fetch_array($res);
+	$sql = fnSqlArticleList(1, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleNote, $sKeyBox, $sDrawing, $sSellCharge, $sPage, $orderBy, $orderTo);
+	$res = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_array($res);
 
 	$count = $row[0];
 
@@ -108,7 +108,7 @@ function subArticle()
 			</tr>
 			<?php
 			$sql = fnSqlArticleList(1, $sDel, $sArticle, $sRoom, $sKeyPlace, $sArticleNote, $sKeyBox, $sDrawing, $sSellCharge, $sPage, $orderBy, $orderTo);
-
+			var_dump($sql);
 			$res = mysqli_query($conn, $sql);
 			$i = 0;
 			while ($row = mysqli_fetch_array($res)) {
@@ -307,7 +307,7 @@ function subArticleEditComplete()
 		$res = mysqli_query($conn, $sql);
 	} else {
 		// 新規登録
-		$sql = fnSqlArticleInsert(fnNextNo('ARTICLE'), $article, $room, $keyPlace, $address, $articleNote, $keyBox, $drawing, $sellCharge, $del);
+		$sql = fnSqlArticleInsert(fnNextNo('ARTICLE'), $keyPlace, $article, $address,  $keyBox, $articleNote, $drawing, $sellCharge, $room, $del);
 
 		$res = mysqli_query($conn, $sql);
 
